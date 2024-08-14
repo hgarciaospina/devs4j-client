61a9cf3c43a909ec44f62d75d455149f678f6d25
Henry García <henrygarciaospina@gmail.com>
2024-08-13 20:17:34 -0500

    Review, are reading the properties from the client application, not from the server application that handles the configuration of the environments.

--- src/main/java/com/devs4j/dragonball/controller/ApplicationNameController.java
+++ src/main/java/com/devs4j/dragonball/controller/ApplicationNameController.java
@@ -1,5 +1,6 @@
 package com.devs4j.dragonball.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,19 +9,25 @@ import org.springframework.web.bind.annotation.RestController;
 import com.devs4j.dragonball.config.DragonBallConfig;
 
 @RestController
-@RequestMapping("/application-name")
+@RequestMapping("/application")
 public class ApplicationNameController {
 
-	private final DragonBallConfig configuration;
-	
-	public ApplicationNameController(DragonBallConfig configuration) {
-		this.configuration = configuration;
-	}
+    private final DragonBallConfig configuration;
 
+    @Value("${spring.profiles.active}")
+    private String activeProfile;
 
-	@GetMapping
-	public ResponseEntity<String> getAppName() {
-		return ResponseEntity.ok(configuration.getName());
-	}
+    public ApplicationNameController(DragonBallConfig configuration) {
+        this.configuration = configuration;
+    }
 
+    @GetMapping("/name")
+    public ResponseEntity<String> getAppName() {
+        return ResponseEntity.ok(configuration.getName());
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<String> getActiveProfile() {
+        return ResponseEntity.ok(activeProfile);
+    }
 }
